0f5815102c6a562ecebc9cc86008a4be7fcb4d90
wenhao <1746594617@qq.com>
2022-09-05 08:25:49 +0800

    add bottom

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="docker">
+    <span class="docker__item docker__item--active">
+      <div class="iconfont">&#xe600;</div>
+      <div class="docker__title">首页</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe62f;</div>
+      <div class="docker__title">购物车</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe7c5;</div>
+      <div class="docker__title">订单</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe661;</div>
+      <div class="docker__title">我的</div>
+    </span>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.docker {
+  box-sizing: border-box;
+  display: flex;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  padding: 0 0.12rem;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  background-color: #09f;
+&__item {
+  flex: 1;
   text-align: center;
-  color: #2c3e50;
+  .iconfont {
+    margin: 0.07rem 0 0.02rem 0;
+    font-size: 0.2rem;
+  }
+  &--active{
+    color: #1fa4fc;
+  }
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+&__title{
+    font-size: .2rem;
+    transform: scale(.5,.5);
+    transform-origin: center top;
   }
 }
 </style>
