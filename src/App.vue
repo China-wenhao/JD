1adaf555663bb21c293361d39cffb9223ec1a950
wenhao <1746594617@qq.com>
2022-09-05 12:12:32 +0800

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe608;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="iconfont position_notice">&#xe64e;</span>
+    </div>
+  </div>
   <div class="docker">
     <span class="docker__item docker__item--active">
       <div class="iconfont">&#xe600;</div>
@@ -20,6 +27,36 @@
 </template>
 
 <style lang="scss">
+  @import './style/viriables.scss';
+  @import './style/mixins.scss';
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0.5rem;
+  right: 0;
+  // background: #09f;
+  padding: 0 0.18rem;
+}
+.position {
+  position: relative;
+  padding: .16rem .24rem .16rem 0;
+  line-height: .22rem;
+  font-size: .16rem;
+  @include ellipse;
+  .position__icon {
+    position: relative;
+    top: .01rem;
+    font-size: .2rem;
+  }
+  .position_notice {
+    position: absolute;
+    right: 0;
+    top: .17rem;
+    font-size: .2rem;
+  }
+  color:$content-font-color;
+}
 .docker {
   box-sizing: border-box;
   display: flex;
@@ -29,22 +66,23 @@
   padding: 0 0.12rem;
   width: 100%;
   height: 0.49rem;
-  border-top: 1px solid #f1f1f1;
+  border-top: 0.01rem solid #f1f1f1;
   // background-color: #09f;
-&__item {
-  flex: 1;
-  text-align: center;
-  .iconfont {
-    margin: 0.07rem 0 0.02rem 0;
+  color:$content-font-color;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.2rem;
+    }
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+  &__title {
     font-size: 0.2rem;
-  }
-  &--active{
-    color: #1fa4fc;
-  }
-}
-&__title{
-    font-size: .2rem;
-    transform: scale(.5,.5);
+    transform: scale(0.5, 0.5);
     transform-origin: center top;
   }
 }
